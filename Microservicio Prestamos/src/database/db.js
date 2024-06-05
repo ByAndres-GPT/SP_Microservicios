6251bceaf349b6b9258824b2e835ffbe5cf85890
ByAndres-GPT <baudilioandresabso@ufps.edu.co>
2024-06-05 10:56:02 -0500

    db.js

--- Microservicio Prestamos/src/database/db.js
+++ Microservicio Prestamos/src/database/db.js
@@ -3,6 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.MYSQL_URL, {define: {timestamps: false}});
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    define: {
+        timestamps: false
+    }
+});
+
 
 export default sequelize;
